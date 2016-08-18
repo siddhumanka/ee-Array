38d4ae1315ff921b5db96e485b809eb75011cb96
siddhumanka <siddhu.manka94@gmail.com>
2016-08-18 14:32:41 +0530

    1st commit

--- src/ArrayProblems.java
+++ src/ArrayProblems.java
@@ -1,6 +1,23 @@
 
 
 public class ArrayProblems {
-
-
+    public static int hammingDistance(int[] aList, int[] bList){
+        int distance=0;
+        int maxLen=Math.max(aList.length,bList.length);
+        int extraNumbers = maxLen-Math.min(aList.length,bList.length);
+        for(int i=0;i<Math.min(aList.length,bList.length);i++){
+            if(aList[i]!=bList[i]){
+                distance++;
+            }
+        }
+        return distance + extraNumbers;
+    }
+    public static void main(String [] argv){
+        int [] aList = {1, 2, 0 ,4, 5, 4, 3, 9, 1};
+        int [] bList = {1, 2, 8, 4, 5, 4, 3, 5, 1};
+        System.out.println("Hamming distance between array" +
+                " {1, 2, 3, 4, 5, 4, 3, 2, 1} and " +
+                "{1, 2, 8, 4, 5, 4, 3, 5, 1} is "+
+                ArrayProblems.hammingDistance(aList,bList));
+    }
 }
