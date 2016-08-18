4e60d3dc55990dd8cce8a29e85e97f90aced5e5f
siddhumanka <siddhu.manka94@gmail.com>
2016-08-18 14:46:41 +0530

    3rd commit

--- src/ArrayProblems.java
+++ src/ArrayProblems.java
@@ -2,21 +2,25 @@ import java.util.Arrays;
 
 public class ArrayProblems {
     public static int hammingDistance(int[] aList, int[] bList){
+        if(aList.length!=bList.length){
+            return -1;
+        }
         int distance=0;
-        int maxLen=Math.max(aList.length,bList.length);
-        int extraNumbers = maxLen-Math.min(aList.length,bList.length);
-        for(int i=0;i<Math.min(aList.length,bList.length);i++){
+        for(int i=0;i<aList.length;i++){
             if(aList[i]!=bList[i]){
                 distance++;
             }
         }
-        return distance + extraNumbers;
+        return distance;
     }
     public static void main(String [] argv){
         int [] aList = {1, 2, 0 ,4, 5, 4, 3, 9, 1};
         int [] bList = {1, 2, 8, 4, 5, 4, 3, 5, 1};
-        System.out.println("Hamming distance between array" +
-                Arrays.toString(aList)+" and array"+Arrays.toString(bList)+" is "+
-                ArrayProblems.hammingDistance(aList,bList));
+        int distance =ArrayProblems.hammingDistance(aList,bList);
+        if(distance>=0) {
+            System.out.println("Hamming distance between array" +
+                    Arrays.toString(aList) + " and array" + Arrays.toString(bList) + " is " +distance);
+        }
+        else System.out.println("Unequal Lengths of arrays ");
     }
 }
